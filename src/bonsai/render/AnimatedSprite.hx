bc7e13ddaf80af885970d4794544f29e0e245eb0
Daniel B <danielblaker@protonmail.com>
2020-01-20 18:49:59 +1100

    Start events and terrible animation update.

--- src/bonsai/render/AnimatedSprite.hx
+++ src/bonsai/render/AnimatedSprite.hx
@@ -10,6 +10,8 @@ class AnimatedSprite {
 	var frame:Int;
 	var spriteMap:SpriteMap;
 
+	var tickCounter = 0;
+
 	public function new (spriteMap:SpriteMap) {
 		this.spriteMap = spriteMap;
 		animations = new Map<String, Animation>();
@@ -21,10 +23,17 @@ class AnimatedSprite {
 		playing = identifier;
 		frame = 0;
 	}
+	public function update (dt) {
+		tickerCounter++;
+		if (tickCounter > 99) {
+			tickerCounter = 0;
+			frame+= 1;
+			if (frame > animations.get(playing).length) - 1)
+				frame = 0;
+	}
 	public function render (canvas:kha.graphics2.Graphics, x, y) {
 		var currentAnimation = animations.get(playing);
 		var currentFrame = currentAnimation.frames[frame];
 		spriteMap.render(canvas, x, y, currentFrame);
-
 	}
 }
