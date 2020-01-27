bbed6f50c0b28d9d4adb25381f6249d9629e3ef7
Jordan <Jordansplat@gmail.com>
2020-01-27 18:21:26 +1100

    More sprites and animations

--- src/Main.hx
+++ src/Main.hx
@@ -29,17 +29,36 @@ class Player extends Entity {
 
 	public var width:Int = 16;
 	public var height:Int = 32;
-	var spriteMap:SpriteMap;
+	var sprite:SpriteMap;
+	var walkingSpriteMap:SpriteMap;
+	var animation:AnimatedSprite;
+	var transformation:Transformation;
 
 	override public function new (x, y) {
 		super();
 		this.position.x = x;
 		this.position.y = y;
-		this.spriteMap = new SpriteMap(kha.Assets.images.player, this.width, this.height);
+		this.sprite = new SpriteMap(kha.Assets.images.player, this.width, this.height);
+		this.walkingSpriteMap = new SpriteMap(kha.Assets.images.playerWalk, this.width, this.height);
+		this.animation = new AnimatedSprite(walkingSpriteMap);
+		this.animation.registerAnimation("walk", {frames:[0,1,2,3,4,5,6,7,8,9,10,11,12]});
+		this.animation.play("walk");
+		this.transformation = new Transformation();
 	}
 
 	override public function render (graphics:kha.graphics2.Graphics) {
-		this.spriteMap.render(graphics, this.position.x, this.position.y, 1);
+		this.transformation.offset = new kha.math.Vector2(this.position.x, this.position.y);
+		this.transformation.scale = new kha.math.Vector2(8, 8);
+		this.transformation.origin = new kha.math.Vector2(this.width/2, this.height);
+
+		this.transformation.apply(graphics);
+		this.animation.render(graphics, 0, 0);
+		// this.sprite.render(graphics, 0, 0, 1);
+		this.transformation.finish(graphics);
+	}
+
+	override public function update (dt){
+		this.animation.update(dt);
 	}
 }
 
@@ -59,18 +78,18 @@ class Goblin extends Entity {
 		this.sprite = new SpriteMap(kha.Assets.images.goblin, this.width, this.height);
 		this.spriteMap = new SpriteMap(kha.Assets.images.goblinRunSheet, this.width, this.height);
 		this.animation = new AnimatedSprite(this.spriteMap);
-		this.animation.registerAnimation("walk", {frames:[0,1,2,3,4,5 ]});
+		this.animation.registerAnimation("walk", {frames:[0,1,2,3,4,5]});
 		this.animation.play("walk");
 		this.transformation = new Transformation();
 	}
 
 	override public function render (graphics:kha.graphics2.Graphics) {
 		this.transformation.offset = new kha.math.Vector2(this.position.x, this.position.y);
-		this.transformation.scale = new kha.math.Vector2(1, 1);
+		this.transformation.scale = new kha.math.Vector2(10, 10);
 		this.transformation.origin = new kha.math.Vector2(this.width/2, this.height);
 
 		this.transformation.apply(graphics);
-		this.animation.render(graphics, 1,0);
+		this.animation.render(graphics, 0, 0);
 		// this.sprite.render(graphics, 0, 0, 1);
 		this.transformation.finish(graphics);
 	}
