9ba8a7c75f5813dbe1b89e55342972b5df8292a7
Jordan <Jordansplat@gmail.com>
2020-01-30 14:53:07 +1100

    Added and implemented Inn Tilemap

--- src/game/TileMap.hx
+++ src/game/TileMap.hx
@@ -11,19 +11,23 @@ class TileMap extends Entity {
 	var tiles:Array<Array<Int>> = [];
 
 	var tiled:bonsai.resource.Tiled;
+	var tiled2:bonsai.resource.Tiled;
 	var spriteMap:bonsai.render.SpriteMap;
+	var spriteMap2:bonsai.render.SpriteMap;
 
 	override public function new () {
 		super();
 
-		tiled = new bonsai.resource.Tiled(kha.Assets.blobs.level_tmx.toString());
-		width = tiled.width;
-		height = tiled.height;
+		tiled = new bonsai.resource.Tiled(kha.Assets.blobs.levelDanny_tmx.toString());
+		tiled2 = new bonsai.resource.Tiled(kha.Assets.blobs.levelInn_tmx.toString());
+		width = tiled2.width;
+		height = tiled2.height;
 
 		spriteMap = new SpriteMap(kha.Assets.images.tiles, 4, 4);
+		spriteMap2 = new SpriteMap(kha.Assets.images.tiles2, 16, 16);
 
 		this.transformation = new Transformation();
-		this.transformation.offset = new kha.math.Vector2(0,20);
+		this.transformation.offset = new kha.math.Vector2(0,0);
 		this.transformation.origin = new kha.math.Vector2(0,0);
 	}
 
@@ -32,7 +36,8 @@ class TileMap extends Entity {
 		
 		for (y in 0...width) {
 			for (x in 0...height) {
-				spriteMap.render(graphics, x*4, y*4, tiled.tiles[y][x] - 1);
+				// spriteMap.render(graphics, x*4, y*4, tiled.tiles[y][x] - 1);
+				spriteMap2.render(graphics, x*16, y*16, tiled2.tiles[y][x] - 1);
 			}
 		}
 
